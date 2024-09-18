c602b5c17404e75600e6a96a3efa14700b4335cb
DmitryZamarekhin <dim.zam1986@gmail.com>
2024-09-18 20:39:56 +0200

    added endpoint for notes

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Security
+from fastapi.security.api_key import APIKeyHeader
 
+from db.db import get_all_notes
+from db.db import get_user
 
 app = FastAPI()
+authorization = APIKeyHeader(name="Authorization")
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return "Hello production!"
+
+
+@app.get("/notes")
+def read_notes(api_token: str = Security(authorization)):
+    user = get_user(api_token)
+    if user:
+        return get_all_notes(user)
+    return []
