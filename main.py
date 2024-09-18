274103ba94b4d66233ff2d5d3dfb30c9cb36a494
DmitryZamarekhin <dim.zam1986@gmail.com>
2024-09-18 22:36:47 +0200

    added yandex speller

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.security.api_key import APIKeyHeader
 from db.db import get_all_notes
 from db.db import get_user
 from db.db import add_note
+from y_speller import spell_check
 
 app = FastAPI()
 authorization = APIKeyHeader(name="Authorization")
@@ -26,6 +27,7 @@ def read_notes(api_token: str = Security(authorization)):
 def add_notes(note_data: str, api_token: str = Security(authorization)):
     user = get_user(api_token)
     if user:
+        note_data = spell_check(note_data)
         if add_note(user, text_note=note_data):
             return {"message": "Note added successfully"}
         raise HTTPException(status_code=500, detail="Failed to add note")
---
+++ y_speller.py
@@ -0,0 +1,17 @@
+import requests
+
+speller_url = "https://speller.yandex.net/services/spellservice.json/checkText?text="
+
+
+def spell_check(text: str) -> str:
+    res = requests.get(speller_url + text)
+    if res.status_code == 200:
+        wrong_words = res.json()
+        if wrong_words:
+            wrong_words.reverse()
+            for wrong_word in wrong_words:
+                start = wrong_word["pos"],
+                end = wrong_word["pos"] + wrong_word["len"]
+                text = text[:start] + wrong_word["s"][0] + text[end:]
+    return text
+
