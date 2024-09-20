d5a9aa00a2abafbde50a602929982c24b98a2a83
DmitryZamarekhin <dim.zam1986@gmail.com>
2024-09-20 13:49:52 +0200

    added async for add_notes

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def read_notes(api_token: str = Security(authorization)) -> list[NoteData]:
 
 
 @app.post("/notes")
-def add_notes(note_data: NoteData, api_token: str = Security(authorization)):
+async def add_notes(note_data: NoteData, api_token: str = Security(authorization)):
     user = get_user(api_token)
     if user:
-        note_data = spell_check(note_data.text_note)
+        note_data = await spell_check(note_data.text_note)
         if add_note(user, text_note=note_data):
             return {"message": "Note added successfully"}
         raise HTTPException(status_code=500, detail="Failed to add note")
