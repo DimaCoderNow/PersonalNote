17d4d95998cb4263b15546f309a82248aac26a44
DmitryZamarekhin <dim.zam1986@gmail.com>
2024-09-20 13:29:49 +0200

    added async for spell_check

--- y_speller.py
+++ y_speller.py
@@ -1,16 +1,18 @@
-import requests
+import aiohttp
+
 
 speller_url = "https://speller.yandex.net/services/spellservice.json/checkText?text="
 
 
-def spell_check(text: str) -> str:
-    res = requests.get(speller_url + text)
-    if res.status_code == 200:
-        wrong_words = res.json()
-        if wrong_words:
-            wrong_words.reverse()
-            for wrong_word in wrong_words:
-                start = wrong_word["pos"]
-                end = wrong_word["pos"] + wrong_word["len"]
-                text = text[:start] + wrong_word["s"][0] + text[end:]
+async def spell_check(text: str) -> str:
+    async with aiohttp.ClientSession() as session:
+        async with session.get(speller_url + text) as res:
+            if res.status == 200:
+                wrong_words = await res.json()
+                if wrong_words:
+                    wrong_words.reverse()
+                    for wrong_word in wrong_words:
+                        start = wrong_word["pos"]
+                        end = wrong_word["pos"] + wrong_word["len"]
+                        text = text[:start] + wrong_word["s"][0] + text[end:]
     return text
